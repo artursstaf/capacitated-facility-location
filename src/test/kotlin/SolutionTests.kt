7d8e56b0b75ded438222bda0c5a109bf0e097056
Artūrs <arturs.stafanovics@gmail.com>
2022-10-24 14:39:13 +0300

    Add test execution time println

--- src/test/kotlin/SolutionTests.kt
+++ src/test/kotlin/SolutionTests.kt
@@ -5,47 +5,52 @@ internal class SolutionTests {
 
     @Test
     fun testBasic() {
+        val start = System.currentTimeMillis()
         val input = SolutionTests::class.java.getResource("test_input_1.json")!!.readText()
         val model = Model.fromJsonString(input)
         val solver = Solver(model)
-
         assertEquals(6620, solver.solveLAHC())
+        println("Execution time ${(System.currentTimeMillis() - start)/1000.0}s")
     }
 
     @Test
     fun testConvergeLowerWarehouseCost() {
+        val start = System.currentTimeMillis()
         val input = SolutionTests::class.java.getResource("test_input_2.json")!!.readText()
         val model = Model.fromJsonString(input)
         val solver = Solver(model)
-
         assertEquals(2880, solver.solveLAHC())
+        println("Execution time ${(System.currentTimeMillis() - start)/1000.0}s")
     }
 
     @Test
     fun testConvergeLowerTransportationCost() {
+        val start = System.currentTimeMillis()
         val input = SolutionTests::class.java.getResource("test_input_3.json")!!.readText()
         val model = Model.fromJsonString(input)
         val solver = Solver(model)
-
         assertEquals(1940, solver.solveLAHC())
+        println("Execution time ${(System.currentTimeMillis() - start)/1000.0}s")
     }
 
     @Test
     fun testDoesNotExceedWarehouseCapacity() {
+        val start = System.currentTimeMillis()
         val input = SolutionTests::class.java.getResource("test_input_4.json")!!.readText()
         val model = Model.fromJsonString(input)
         val solver = Solver(model)
-
         assertEquals(5550, solver.solveLAHC())
+        println("Execution time ${(System.currentTimeMillis() - start)/1000.0}s")
     }
 
     @Test
     fun testBig() {
+        val start = System.currentTimeMillis()
         val input = SolutionTests::class.java.getResource("test_input_5.json")!!.readText()
         val model = Model.fromJsonString(input)
         val solver = Solver(model)
-
         assert(solver.solveLAHC(n=50, earlyStopping=500000) < 100000)
+        println("Execution time ${(System.currentTimeMillis() - start)/1000.0}s")
     }
 }
 
